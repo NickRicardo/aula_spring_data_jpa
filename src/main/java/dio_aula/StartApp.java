5928ee4edec0f4c710bc18a1592e8f060414f480
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-07-29 12:10:38 -0300

    Buscas personalizadas

--- src/main/java/dio_aula/StartApp.java
+++ src/main/java/dio_aula/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
 
@@ -14,12 +16,30 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<User> users = repository.findByNameContaining("Keilla");
+
+        for (User u : users) {
+            System.out.println(u);
+        }
+
         User user = new User();
         user.setName("Nícolas");
         user.setUsername("nicollas");
         user.setPassword("dio123");
 
+        User user1 = new User();
+        user1.setName("Keilla");
+        user1.setUsername("keilla");
+        user1.setPassword("dio321");
+
+        User user2 = new User();
+        user2.setName("Laura");
+        user2.setUsername("laura");
+        user2.setPassword("123456");
+
         repository.save(user);
+        repository.save(user1);
+        repository.save(user2);
 
         for (User u : repository.findAll()) {
             System.out.println(u);
--- src/main/java/dio_aula/repository/UserRepository.java
+++ src/main/java/dio_aula/repository/UserRepository.java
@@ -2,6 +2,22 @@ package dio_aula.repository;
 
 import dio_aula.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    //Query Method - Retorna a lista de usuários contendo a parte do name
+    List<User> findByNameContaining(String name);
+
+
+    //Query Override - Retorna a lista de usuários contendo a parte do name
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name") String name);
+
+
+    //Query Method - Retorna um usuário pelo campo username
+    User findByUsername(String username);
 }
